4dd8de919c55c6ab072edb70a4b5e46026aa521c
rbowlus <84033247+rbowlus@users.noreply.github.com>
2021-06-16 09:28:18 -0500

    Delete pract_module directory

--- pract_module/calculate/__init__.py
+++
@@ -1,7 +0,0 @@
-import math
-
-def calc_square_footage(x, y):
-    print(x * y)
-
-def circum_circ(r):
-    print(2 * math.pi * r)
--- pract_module/main.py
+++
@@ -1,5 +0,0 @@
-from calculate import calc_square_footage, circum_circ
-
-calc_square_footage(2,2)
-
-circum_circ(4)
