cf186b62187b39e95459ce45bbd98628111df393
rbowlus <84033247+rbowlus@users.noreply.github.com>
2021-06-16 09:28:48 -0500

    Add files via upload

---
+++ pract_module/main.py
@@ -0,0 +1,5 @@
+from calculate import calc_square_footage, circum_circ
+
+calc_square_footage(x = int(input('Enter number:\n')), y= int(input('Enter number:\n')))
+
+circum_circ(r = int(input('Enter number:\n')))
